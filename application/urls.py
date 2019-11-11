0a9e9e3c819c295ef3b02470e6b916f50e825461
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-11-11 13:50:18 +0100

    Fixing the csrf token

--- application/urls.py
+++ application/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
     path('index/login/', views.LoginView.as_view(), name='login'),
-    path('index/login/login_user',views.loginUser, name='loginUser')
+    path('index/login/login_user/',views.loginUser, name='loginUser'),
 ]
