6653869520efa797fd09e67627c615adbcb0ee73
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-11-11 11:53:15 +0100

    Starting implementing login form

--- application/urls.py
+++ application/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('index/login/', views.LoginView.as_view(), name='login'),
+    path('index/login/login_user',views.loginUser, name='loginUser')
 ]
--- application/views.py
+++ application/views.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
+
 # Create your views here.
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'application/index.html'
+
+    def get_queryset(self):
+        return "salam"
+
+class LoginView(generic.ListView):
+    template_name = 'application/login.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
+    #             albums = Album.objects.filter(user=request.user)
+    #             return render(request, 'music/index.html', {'albums': albums})
+    #         else:
+    #             return render(request, 'music/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'application/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'application/login.html')
