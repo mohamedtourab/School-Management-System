f473ac778060479a56eceb720e7cba85f142666c
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-11-17 13:22:56 +0100

    Addin first name and last name and email in the model

--- application/models.py
+++ application/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 # Create your models here.
 
 class Principle(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
     ID = models.AutoField(primary_key=True)
 
     def __str__(self):
@@ -16,6 +16,9 @@ class Principle(models.Model):
 
 class AdministrativeOfficer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
     ID = models.AutoField(primary_key=True)
 
     def __str__(self):
@@ -50,6 +53,9 @@ class Course(models.Model):
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     ID = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
     fiscalCode = models.CharField(max_length=16)
     coordinatedClass = models.ForeignKey(ClassInfo, on_delete=models.CASCADE, blank=True, null=True)
 
