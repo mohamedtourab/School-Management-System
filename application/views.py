0296eec1231c5c2bbc2517bba11b60a6b7ea4805
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-11-24 13:19:03 +0100

    Formatting the view file

--- application/views.py
+++ application/views.py
@@ -8,162 +8,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect  # , render_to_response
 from django.urls import reverse
 
-from .models import StudentCourse, PerformanceGrade, Parent, Content, Course, Student, ClassInfo, TeacherCourse, ParentStudent
+from .models import StudentCourse, PerformanceGrade, Parent, Content, Course, Student, ClassInfo, TeacherCourse, \
+    ParentStudent
 from django.views import generic
 from application.forms import StudentForm, ParentSignUpForm, ClassComposeForm, ContentForm, PerformanceGradeForm
 
 
 # Create your views here.
 
-class TestView(generic.ListView):
-    template_name = 'parent/afterloginparent.html'
-
-    def get_queryset(self):
-        return "salam"
-
+# -----------------------------------------------------------------------------------------------
+####### ADMINISTRATIVE OFFICER AREA##########
+# -----------------------------------------------------------------------------------------------
 
 class AdministrativeOfficer(generic.ListView):
     template_name = 'administrativeOfficer/base.html'
 
     def get_queryset(self):
         return "salam"
-
-
-class CourseView(generic.ListView):
-    template_name = 'parent/course.html'
-
-    def get_queryset(self):
-        return "salam"
-
-
-class CourseDetailView(generic.ListView):
-    template_name = 'parent/course.html'
-    context_object_name = 'topics'
-
-    def get_queryset(self):
-        return Content.objects.filter(courseID=self.kwargs['courseID'])
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(CourseDetailView, self).get_context_data(**kwargs)
-        context['courseDetails'] = Course.objects.get(ID=self.kwargs['courseID'])
-        return context
-
-
-
-class ParentView(generic.ListView):
-    template_name = 'parent/afterloginparent.html'
-    context_object_name = 'allStudentCourses'
-
-    def get_queryset(self):
-        return Course.objects.filter(studentcourse__studentID=self.kwargs['studentID'])  # GET STUDENT ID HERE
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(ParentView, self).get_context_data(**kwargs)
-        context['studentID'] = self.kwargs['studentID']
-        context['parentStudent'] = ParentStudent.objects.filter(studentID=self.kwargs['studentID'])
-        context['studentClass'] = ClassInfo.objects.get(student__ID=self.kwargs['studentID'])
-        return context
-
-class ChooseChild(generic.ListView):
-    template_name = 'parent/chooseChild.html'
-    context_object_name = 'allChildren'
-
-    def get_queryset(self):
-        return ParentStudent.objects.filter(parentID=self.request.user.parent.ID)  # GET STUDENT ID HERE
-
-class ParentAttendanceView(generic.ListView):
-    template_name = 'parent/attendancep.html'
-
-    def get_queryset(self):
-        return "salam"
-
-class ParentGradeView(generic.ListView):
-    template_name = 'parent/gradep.html'
-    context_object_name = 'allGrades'
-
-    def get_queryset(self):
-        return PerformanceGrade.objects.filter(studentCourseID__studentID=self.kwargs[
-            'studentID'])  # GET STUDENTCOURSEID HERE ; should I send studentID to the url here?
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(ParentGradeView, self).get_context_data(**kwargs)
-        context['studentID'] = self.kwargs['studentID']
-        context['studentCourse'] = StudentCourse.objects.filter(
-            studentID=self.kwargs['studentID'])  # GET STUDENT ID HERE
-
-        columns = 0
-        for studentcourse in context['studentCourse']:
-            gradeCounter = 0
-            for grade in context['allGrades']:
-                if grade.studentCourseID.courseID.name == studentcourse.courseID.name:
-                    gradeCounter += 1
-            if gradeCounter > columns:
-                columns = gradeCounter
-        context['columns'] = range(columns)
-        return context
-
-
-class IndexView(generic.ListView):
-    template_name = 'application/index.html'
-
-    def get_queryset(self):
-        return "salam"
-
-
-class LoginView(generic.ListView):
-    template_name = 'application/login.html'
-
-    def get_queryset(self):
-        return "salam"
-
-
-def logout_view(request):
-    logout(request)
-    return redirect('application:index')
-
-
-def loginUser(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                try:
-                    teacher = user.teacher
-                    return redirect('application:teacher')
-                except:
-                    try:
-                        parent = user.parent
-                        numberOfStudent = ParentStudent.objects.filter(parentID=parent.ID).count()
-
-
-                        if parent.lastLogin is False:
-                            parent.lastLogin = True
-                            parent.save()
-                            return redirect('application:change_password')
-                        else:
-                            if (numberOfStudent == 1):
-                                studentID = ParentStudent.objects.get(parentID=parent.ID).studentID.ID
-                                return redirect('application:parentWithID', studentID)
-                            else:
-                                return redirect('application:chooseChild')
-                        # return render(request,'parent/afterloginparent.html', {'studentID': studentID, 'studentCourses': studentCourses})
-                    except:
-                        try:
-                            administrativeOfficer = user.administrativeofficer
-                            return redirect('application:administrativeOfficer')
-                        except:
-                            try:
-                                principle = user.principle
-                                return HttpResponse("<h1>you are logged in as principle</h1>")
-                            except:
-                                return HttpResponse("<h1>you are a hacker</h1>")
-        else:
-            return render(request, 'application/login.html', {'error_message': 'Invalid login'})
-    else:
-        return render(request, 'application/login.html')
 
 
 def enrollStudent(request):
@@ -241,7 +102,7 @@ def parentSignup(request):
                 user.refresh_from_db()  # load the profile instance created by the signal
                 parent = Parent.objects.create(user=user, )
                 for student in form.cleaned_data.get('studentID'):
-                    ParentStudent.objects.create(studentID=student,parentID=parent)
+                    ParentStudent.objects.create(studentID=student, parentID=parent)
 
                 messages.success(request, 'Parent has been successfully added')
 
@@ -267,6 +128,92 @@ def sendmailtoparent(username, email, password):
               fail_silently=False)
 
 
+# -----------------------------------------------------------------------------------------------
+####### PARENT AREA##########
+# -----------------------------------------------------------------------------------------------
+class TestView(generic.ListView):
+    template_name = 'parent/afterloginparent.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+class CourseView(generic.ListView):
+    template_name = 'parent/course.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+class CourseDetailView(generic.ListView):
+    template_name = 'parent/course.html'
+    context_object_name = 'topics'
+
+    def get_queryset(self):
+        return Content.objects.filter(courseID=self.kwargs['courseID'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['courseDetails'] = Course.objects.get(ID=self.kwargs['courseID'])
+        return context
+
+
+class ParentView(generic.ListView):
+    template_name = 'parent/afterloginparent.html'
+    context_object_name = 'allStudentCourses'
+
+    def get_queryset(self):
+        return Course.objects.filter(studentcourse__studentID=self.kwargs['studentID'])  # GET STUDENT ID HERE
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ParentView, self).get_context_data(**kwargs)
+        context['studentID'] = self.kwargs['studentID']
+        context['parentStudent'] = ParentStudent.objects.filter(studentID=self.kwargs['studentID'])
+        context['studentClass'] = ClassInfo.objects.get(student__ID=self.kwargs['studentID'])
+        return context
+
+
+class ChooseChild(generic.ListView):
+    template_name = 'parent/chooseChild.html'
+    context_object_name = 'allChildren'
+
+    def get_queryset(self):
+        return ParentStudent.objects.filter(parentID=self.request.user.parent.ID)  # GET STUDENT ID HERE
+
+
+class ParentAttendanceView(generic.ListView):
+    template_name = 'parent/attendancep.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+class ParentGradeView(generic.ListView):
+    template_name = 'parent/gradep.html'
+    context_object_name = 'allGrades'
+
+    def get_queryset(self):
+        return PerformanceGrade.objects.filter(studentCourseID__studentID=self.kwargs[
+            'studentID'])  # GET STUDENTCOURSEID HERE ; should I send studentID to the url here?
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ParentGradeView, self).get_context_data(**kwargs)
+        context['studentID'] = self.kwargs['studentID']
+        context['studentCourse'] = StudentCourse.objects.filter(
+            studentID=self.kwargs['studentID'])  # GET STUDENT ID HERE
+
+        columns = 0
+        for studentcourse in context['studentCourse']:
+            gradeCounter = 0
+            for grade in context['allGrades']:
+                if grade.studentCourseID.courseID.name == studentcourse.courseID.name:
+                    gradeCounter += 1
+            if gradeCounter > columns:
+                columns = gradeCounter
+        context['columns'] = range(columns)
+        return context
+
+
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
@@ -282,6 +229,10 @@ def change_password(request):
     else:
         return render(request, 'parent/change_password.html', {'form': PasswordChangeForm(user=request.user)})
 
+
+# -----------------------------------------------------------------------------------------------
+####### TEACHER AREA##########
+# -----------------------------------------------------------------------------------------------
 
 class TeacherView(generic.ListView):
     template_name = 'teacher/teacherAfterLogin.html'
@@ -324,4 +275,72 @@ def gradeForm(request, courseID):
             return redirect('application:teacher')
     else:
         form = PerformanceGradeForm(courseID=courseID)
-    return render(request, 'teacher/grade.html', {'form':form,'courseID':courseID,})
+    return render(request, 'teacher/grade.html', {'form': form, 'courseID': courseID, })
+
+
+# -----------------------------------------------------------------------------------------------
+####### GENERAL AREA##########
+# -----------------------------------------------------------------------------------------------
+
+class IndexView(generic.ListView):
+    template_name = 'application/index.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+class LoginView(generic.ListView):
+    template_name = 'application/login.html'
+
+    def get_queryset(self):
+        return "salam"
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('application:index')
+
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                try:
+                    teacher = user.teacher
+                    return redirect('application:teacher')
+                except:
+                    try:
+                        parent = user.parent
+                        numberOfStudent = ParentStudent.objects.filter(parentID=parent.ID).count()
+
+                        if parent.lastLogin is False:
+                            parent.lastLogin = True
+                            parent.save()
+                            return redirect('application:change_password')
+                        else:
+                            if (numberOfStudent == 1):
+                                studentID = ParentStudent.objects.get(parentID=parent.ID).studentID.ID
+                                return redirect('application:parentWithID', studentID)
+                            else:
+                                return redirect('application:chooseChild')
+                        # return render(request,'parent/afterloginparent.html', {'studentID': studentID, 'studentCourses': studentCourses})
+                    except:
+                        try:
+                            administrativeOfficer = user.administrativeofficer
+                            return redirect('application:administrativeOfficer')
+                        except:
+                            try:
+                                principle = user.principle
+                                return HttpResponse("<h1>you are logged in as principle</h1>")
+                            except:
+                                return HttpResponse("<h1>you are a hacker</h1>")
+        else:
+            return render(request, 'application/login.html', {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'application/login.html')
+
+####### PRINCIPLE AREA##########
