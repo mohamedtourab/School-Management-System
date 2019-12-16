ce261554f41aeeb3fa67e2b15b971724f4461439
Elnur <eerzayev95@gmail.com>
2019-12-16 14:43:14 +0100

    Fixing designs for teacher pages

--- application/views.py
+++ application/views.py
@@ -492,6 +492,7 @@ class TeacherView(generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(TeacherView, self).get_context_data(**kwargs)
         context['teacherID'] = self.request.user.teacher.ID
+        context['coordinatedClass'] = Teacher.objects.get(ID=self.request.user.teacher.ID).coordinatedClass
         return context
 
 
